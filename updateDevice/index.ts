5227290aafe942514bede19a99a03b9e67469434
quotto <5211139+quotto@users.noreply.github.com>
2023-03-09 15:13:20 +0000

    apply multiple data

--- updateDevice/index.ts
+++ updateDevice/index.ts
@@ -2,11 +2,13 @@ import { Context, HttpRequest } from "@azure/functions";
 module.exports = function (context: Context, input: any) {
     context.log('Document:', JSON.stringify(input));
     context.log(JSON.stringify(context.bindings.inputDocument));
-    const updateData = {
-        id: input[0].deviceID,
-        scanedTime: input[0].scanedTime,
-        originID: input[0].originID
-    }
+    const updateData = input.map((doc: any)=>{
+        return {
+            id: doc.deviceID,
+            scanendTime: doc.scanendTime,
+            originID: doc.originID
+        }
+    });
     context.bindings.outputDocument = JSON.stringify(updateData);
     context.done();
 };
