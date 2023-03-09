026a342e86171bc45db57275530bbdaf91ec0d28
quotto <5211139+quotto@users.noreply.github.com>
2023-03-09 14:58:42 +0000

    fix

--- updateDevice/index.ts
+++ updateDevice/index.ts
@@ -3,9 +3,9 @@ module.exports = function (context: Context, input: any) {
     context.log('Document:', JSON.stringify(input));
     context.log(JSON.stringify(context.bindings.inputDocument));
     const updateData = {
-        id: input.deviceID,
-        scanedTime: input.scanedTime,
-        originID: input.originID
+        id: input[0].deviceID,
+        scanedTime: input[0].scanedTime,
+        originID: input[0].originID
     }
     context.bindings.outputDocument = JSON.stringify(updateData);
     context.done();
